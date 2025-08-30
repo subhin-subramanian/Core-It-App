3cf8f1c8bc7ab523f59734e15856bbe0ea671058
subhin-subramanian <subhinsubramanian@gmail.com>
2025-08-30 21:28:47 +0530

    Build commands, Frontend static rendering lines added for deployment

--- backend/index.js
+++ backend/index.js
@@ -3,8 +3,6 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
 import path from 'path';
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
 import uploadRouter from './routes/upload.route.js';
 import cookieParser from 'cookie-parser';
 import cartRouter from './routes/cart.route.js';
@@ -13,28 +11,17 @@ import Razorpay from 'razorpay';
 import paymentRouter from './routes/payment.route.js';
 import cors from 'cors';
 
-const app = express();
-
 dotenv.config(); // Loading environment variables
 
-//setup for __dirname and __filename
-// This is necessary because __dirname and __filename are not available in ES modules
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const app = express();
+const __dirname = path.resolve(); // For rendering static files
 
 
+// Middlewares
 app.use(express.json({limit:"10mb"}));
-
 app.use(cors());  // Required after deployment
-
 app.use(cookieParser());
-
 app.use(express.urlencoded({extended:true}));
-
-// Port settings
-app.listen(3000,()=>{
-    console.log('Server running on port 3000');
-});
 
 // DataBase Connection
 mongoose.connect(process.env.MONGO).then(()=>{
@@ -43,8 +30,10 @@ mongoose.connect(process.env.MONGO).then(()=>{
     console.log('Database error: '+error);
 })
 
-// Serving uploaded images statically
-app.use('/uploads',express.static(path.join(__dirname,'uploads')));
+// Port settings
+app.listen(3000,()=>{
+    console.log('Server running on port 3000');
+});
 
 // API endpoints
 app.use('/api/user',userRouter);
@@ -56,4 +45,10 @@ app.use('/api/payment',paymentRouter);
 export const instance = new Razorpay({
     key_id: process.env.RAZOR_PAY_KEY,
     key_secret: process.env.RAZOR_PAY_SECRET
+});
+
+// Frontend static rendering 
+app.use(express.static(path.join(__dirname,'/frontend/dist')));
+app.get('/*name', (req,res)=>{
+    res.sendFile(path.join(__dirname,'frontend','dist','index.html'));
 });
